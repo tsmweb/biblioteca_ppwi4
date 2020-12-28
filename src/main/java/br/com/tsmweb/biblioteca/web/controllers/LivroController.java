af4ed26f9b64a2a9c4eab5af90c9ac977ed841e5
tsmweb <tiago.tsmweb@gmail.com>
2020-12-28 18:52:03 -0300

    Implementação do relatório de livros em PDF.

--- src/main/java/br/com/tsmweb/biblioteca/web/controllers/LivroController.java
+++ src/main/java/br/com/tsmweb/biblioteca/web/controllers/LivroController.java
@@ -1,16 +1,23 @@
 package br.com.tsmweb.biblioteca.web.controllers;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -36,9 +43,15 @@ import br.com.tsmweb.biblioteca.models.repository.filtros.LivroFiltro;
 import br.com.tsmweb.biblioteca.models.repository.pagination.Pagina;
 import br.com.tsmweb.biblioteca.models.service.EditoraService;
 import br.com.tsmweb.biblioteca.models.service.LivroService;
+import br.com.tsmweb.biblioteca.models.service.components.PrintJasperReport;
 import br.com.tsmweb.biblioteca.models.service.exception.NegocioException;
+import br.com.tsmweb.biblioteca.models.service.reports.JasperReportsService;
 import br.com.tsmweb.biblioteca.models.service.reports.LivroReportPdf;
 import br.com.tsmweb.biblioteca.web.response.ResponseSelect2Data;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 
 @Controller
 @RequestMapping(value = "/livro")
@@ -52,6 +65,14 @@ public class LivroController {
 	
 	@Autowired
 	private LivroReportPdf livroReportPdf;
+	
+	@Autowired
+	private JasperReportsService jasperReportsService;
+	
+	@Autowired
+	private PrintJasperReport printJasperReport;
+	
+	private Map<String, Object> params = new HashMap<>();
 	
 	@GetMapping(value = "/listar")
 	public ModelAndView listarLivro(LivroFiltro livroFiltro,
@@ -160,6 +181,45 @@ public class LivroController {
 	public String handlerException(NegocioException ex, RedirectAttributes flash) {
 		flash.addFlashAttribute("error", ex.getMessage());
 		return "redirect:/livro/listar";
+	}
+	
+	@GetMapping(value = "/relpdf")
+	public ResponseEntity<byte[]> imprimeRelatorioPdf() {
+		params.put(JRParameter.REPORT_LOCALE, new Locale("pt", "BR"));
+		params.put("CODIGO_INICIAL", 1L);
+		params.put("CODIGO_FINAL", 500L);
+		
+		printJasperReport.setFile("rel_livros");
+		printJasperReport.setParams(params);
+		
+		byte[] relatorio = jasperReportsService.generateNativeSqlReport(printJasperReport);
+		
+		return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+					.body(relatorio);
+	}
+	
+	@GetMapping(value = "/reldownload")
+	public void downloadRelatorioPdf(HttpServletResponse response) {
+		params.put(JRParameter.REPORT_LOCALE, new Locale("pt", "BR"));
+		params.put("CODIGO_INICIAL", 1L);
+		params.put("CODIGO_FINAL", 500L);
+		
+		printJasperReport.setFile("rel_livros");
+		printJasperReport.setParams(params);
+		
+		response.setContentType("application/x-download");
+		response.setHeader("Content-Disposition", String.format("attachment; filename=\"livros.pdf\""));
+		
+		try {
+			JasperPrint jasperPrint = jasperReportsService.downloadReportPdf(printJasperReport);
+			OutputStream out = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(JRException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
